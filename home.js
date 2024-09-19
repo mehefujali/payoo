a34fa381efeb0ff2b6678cf91a9a4b663d7240f0
Mehefuj Ali <mehefujali30@gmail.com>
2024-09-19 22:38:47 +0530

    cashout fiture added

--- home.js
+++ home.js
@@ -1,11 +1,12 @@
 const addAmount = document.getElementById('add-amount')
 const pinNumber = document.getElementById('pin-number')
+const availableBalance = document.getElementById('available-balance')
 
 // add money 
 
 document.getElementById('add-money-btn').addEventListener('click', function (event) {
       event.preventDefault()
-      const availableBalance = document.getElementById('available-balance')
+
       const newBalance = parseFloat(addAmount.value) + parseFloat(availableBalance.innerText)
 
 
@@ -22,14 +23,14 @@ document.getElementById('add-money-btn').addEventListener('click', function (eve
             if (parseFloat(addAmount.value) < 0 || isNaN(amountValue)) {
 
                   addAmount.classList.add('border-red-500')
-                  addAmount.placeholder = 'Invalid amount'
+                  addAmount.classList.add('text-red-500')
             }
             if (pinNumber.value !== '1234' || pinNumber.value === false) {
                   pinNumber.classList.add('border-red-500')
-                  pinNumber.placeholder = 'Invalid pin'
+
+                  pinNumber.classList.add('text-red-500')
             }
-            addAmount.value = ''
-            pinNumber.value = ''
+
 
       }
 
@@ -42,17 +43,68 @@ document.getElementById('log-out').addEventListener('click', function () {
 document.getElementById('add-amount').addEventListener('keyup', function () {
       addAmount.classList.remove('border-red-500')
       pinNumber.classList.remove('border-red-500')
-      addAmount.placeholder = 'Add amount'
-      pinNumber.placeholder = 'Pin number'
+      addAmount.classList.remove('text-red-500')
+      pinNumber.classList.remove('text-red-500')
 
 })
 document.getElementById('pin-number').addEventListener('keyup', function () {
       addAmount.classList.remove('border-red-500')
       pinNumber.classList.remove('border-red-500')
-      addAmount.placeholder = 'Add amount'
-      pinNumber.placeholder = 'Pin number'
+      addAmount.classList.remove('text-red-500')
+      pinNumber.classList.remove('text-red-500')
 
 })
 
 
 // cash out 
+const cashOutAmount = document.getElementById('cash-out-amount')
+const cashOutPin = document.getElementById('cas-out-pin-number')
+
+document.getElementById('cash-out-btn').addEventListener('click', function (event) {
+      event.preventDefault()
+      const cashOutAmountValue = parseFloat(cashOutAmount.value)
+      const availableBalanceValue = parseFloat(availableBalance.innerText)
+      console.log(pinNumber.value);
+
+      if (cashOutPin.value === '1234' && cashOutAmountValue <= availableBalanceValue) {
+            const newBalance = availableBalanceValue - cashOutAmountValue;
+            availableBalance.innerText = newBalance
+            cashOutAmount.value = ''
+            cashOutPin.value = ''
+      }
+      else {
+            if (cashOutAmountValue > availableBalanceValue) {
+                  document.getElementById('alert-error-ibalance').classList.remove('hidden')
+                  cashOutAmount.classList.add('text-red-500')
+                  cashOutAmount.classList.add('border-red-500')
+
+
+            }
+            if (isNaN(cashOutAmountValue) === true) {
+                  cashOutAmount.classList.add('text-red-500')
+                  cashOutAmount.classList.add('border-red-500')
+            }
+
+            if (cashOutPin.value !== '1234') {
+                  cashOutPin.classList.add('text-red-500')
+                  cashOutPin.classList.add('border-red-500')
+            }
+      }
+
+})
+cashOutAmount.addEventListener('keyup', function () {
+      cashOutAmount.classList.remove('border-red-500')
+      cashOutPin.classList.remove('border-red-500')
+      cashOutAmount.classList.remove('text-red-500')
+      cashOutPin.classList.remove('text-red-500')
+      document.getElementById('alert-error-ibalance').classList.add('hidden')
+
+
+})
+cashOutPin.addEventListener('keyup', function () {
+      cashOutAmount.classList.remove('border-red-500')
+      cashOutPin.classList.remove('border-red-500')
+      cashOutAmount.classList.remove('text-red-500')
+      cashOutPin.classList.remove('text-red-500')
+
+})

4d391dafaa84edaed7f1c36161715e07caad788e
Mehefuj Ali <mehefujali30@gmail.com>
2024-09-19 20:43:40 +0530

    money add out complite

--- home.js
+++ home.js
@@ -1,6 +1,7 @@
 const addAmount = document.getElementById('add-amount')
 const pinNumber = document.getElementById('pin-number')
 
+
 document.getElementById('add-money-btn').addEventListener('click', function (event) {
       event.preventDefault()
       const availableBalance = document.getElementById('available-balance')
@@ -14,16 +15,40 @@ document.getElementById('add-money-btn').addEventListener('click', function (eve
 
       }
       else {
+            const amountValue = (parseFloat(addAmount.value));
+
+
+            if (parseFloat(addAmount.value) < 0 || isNaN(amountValue)) {
+
+                  addAmount.classList.add('border-red-500')
+                  addAmount.placeholder = 'Invalid amount'
+            }
+            if (pinNumber.value !== '1234' || pinNumber.value === false) {
+                  pinNumber.classList.add('border-red-500')
+                  pinNumber.placeholder = 'Invalid pin'
+            }
             addAmount.value = ''
             pinNumber.value = ''
-            addAmount.classList.add('border-red-500')
-            pinNumber.classList.add('border-red-500')
-            addAmount.placeholder = 'Invalide input'
-            if (pinNumber.value !== '1234') {
-                  pinNumber.placeholder = 'invalid pin'
-            }
 
       }
 
+
+})
+document.getElementById('log-out').addEventListener('click', function () {
+      window.location.href = './index.html'
+
+})
+document.getElementById('add-amount').addEventListener('keyup', function () {
+      addAmount.classList.remove('border-red-500')
+      pinNumber.classList.remove('border-red-500')
+      addAmount.placeholder = 'Add amount'
+      pinNumber.placeholder = 'Pin number'
+
+})
+document.getElementById('pin-number').addEventListener('keyup', function () {
+      addAmount.classList.remove('border-red-500')
+      pinNumber.classList.remove('border-red-500')
+      addAmount.placeholder = 'Add amount'
+      pinNumber.placeholder = 'Pin number'
 
 })

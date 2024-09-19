463e6bbf305391f32a589139f168e9dea334295a
Mehefuj Ali <mehefujali30@gmail.com>
2024-09-19 16:11:02 +0530

    login page created

---
+++ js/login.js
@@ -0,0 +1,30 @@
+const mobileNumber = document.getElementById('mobile-number')
+const pinNumber = document.getElementById('pin-number')
+
+document.getElementById('login-btn').addEventListener('click', function (event) {
+      event.preventDefault()
+
+      // console.log(mobileNumber, pinNumber);
+      if (mobileNumber.value === '8391977901' && pinNumber.value === '1234') {
+            window.location.href = '../home.html'
+      }
+      else {
+            document.getElementById('alert-error').classList.remove('hidden')
+            mobileNumber.classList.add('border-red-600')
+            pinNumber.classList.add('border-red-600')
+
+      }
+
+
+})
+
+mobileNumber.addEventListener('keyup', function () {
+      document.getElementById('alert-error').classList.add('hidden')
+      mobileNumber.classList.remove('border-red-600')
+      pinNumber.classList.remove('border-red-600')
+})
+pinNumber.addEventListener('keyup', function () {
+      document.getElementById('alert-error').classList.add('hidden')
+      mobileNumber.classList.remove('border-red-600')
+      pinNumber.classList.remove('border-red-600')
+})
